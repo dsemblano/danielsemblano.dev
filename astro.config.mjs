7902ffca7c56ccc98d1373466169f5c3c6970a14
Daniel Semblano <dsemblano@gmail.com>
2025-12-09 07:36:27 -0300

    fix astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-site: 'https://danielsemblano.dev'
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: 'https://danielsemblano.dev',
 });
